486f6d0d8b523fcec826ac17854d49c1ed4d00d1
emondewan00 <dewanmohammademon@gmail.com>
2025-01-29 22:15:38 +0600

    setup redux

--- index.js
+++ index.js
@@ -3,7 +3,16 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
+import App from './src/App';
 import {name as appName} from './app.json';
+import React from 'react';
+import {Provider} from 'react-redux';
+import store from './src/store';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => () => (
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+));
